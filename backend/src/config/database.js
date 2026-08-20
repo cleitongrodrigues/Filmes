3d16201e2b437dadb68a9217bc370185d203f1e3
cleitongrodrigues <cleitoungr66@gmail.com>
2026-08-20 20:19:09 -0300

    Apontando para a base de dados

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,6 @@
-const mariadb = require('mariadb');
+const mysql = require('mysql2/promise');
 
-const pool = mariadb.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT || 3306),
   user: process.env.DB_USER,
@@ -8,7 +8,6 @@ const pool = mariadb.createPool({
   database: process.env.DB_NAME,
   connectionLimit: 10,
   multipleStatements: true,
-  trace: true,
   timezone: 'Z'
 });
 
